6f2048c9dd1e0dc43ebc5263205548f9bb26458d
kirthisena <78079354+kirthisena@users.noreply.github.com>
2021-02-03 15:16:17 +0530

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -4,18 +4,20 @@ public class Rectangle extends GeometricObject{
 protected double length, width;
 public Rectangle(){}
 public Rectangle(double l, double w){length= l;width = w;}
-
+	
+// colour of the lines
 public Rectangle(double l, double w, String colour, boolean filled){
 	super(colour,filled);
 	length= l;width = w;
 	}
-public void setLength(double l){length = l;}
+	
+public void setLength(double l){length = l;}// code for length and data type
 public double getLength(){return length;}
-public void setWidth(double w){width = w;}
+public void setWidth(double w){width = w;}// code for width
 public double getWidth(){return width;}
-public double getArea(){return width*length;}
-public double getPerimeter(){return (2*length)*(2*width);}
-public String getShape(){return "Rectangle";}
+public double getArea(){return width*length;}// calculates area
+public double getPerimeter(){return (2*length)*(2*width);}// calculates perimeter
+public String getShape(){return "Rectangle";}// creates a shape output
 public String toString(){return "len is "+length+" width is "+width;}
 
 }
