b30062264c92539caaa638f082bdda66807037e8
Lasal Jayawardena <61999541+LasalJayawardena@users.noreply.github.com>
2021-02-03 15:14:33 +0530

    Added comments for readbility

--- ShapeCollection.java
+++ ShapeCollection.java
@@ -1,5 +1,13 @@
-public interface ShapeCollection {
-public abstract void addShape(GeometricObject shape);
-public abstract void printShapeList();
-public abstract boolean runMenu();
+//An interface is a completely "abstract class" that is used to group related methods with empty bodies
+public interface ShapeCollection 
+{
+  
+    // Method adds a new instance of the shape object to a parent instance variable of a ArrayList type 
+    public abstract void addShape(GeometricObject shape);
+    
+    // Method iterates over the parent instance variable of a ArrayList type ,prints the name, and the type of objjec. eg: V3 Circle
+    public abstract void printShapeList();
+    
+    // Method return boolean   
+    public abstract boolean runMenu();
 }
